4ec83b0292e41b9b58fb2f53f0e766174417793e
mrjulol <julian.thaler06@gmail.com>
2024-03-06 07:02:54 +0100

    :3

---
+++ swifting/Factory.swift
@@ -0,0 +1,82 @@
+//
+//  Singelton.swift
+//  Swifting
+//
+//  Created by Julian Thaler on 05/03/24.
+//
+
+import Foundation
+main()
+
+protocol Motor {
+   func description() -> Void
+}
+
+protocol Wheel {
+   func description() -> Void
+}
+
+protocol CarFactory {
+   func createMotor() -> Motor
+   func createWheels() -> Wheel
+}
+
+class MotorA: Motor {
+   func description() {
+      print("Motor of Car A")
+   }
+}
+
+class MotorB: Motor {
+   func description() {
+      print("Motor of Car B")
+   }
+}
+
+class WheelA: Wheel {
+   func description() {
+      print("Wheel of Car A")
+   }
+}
+
+class WheelB: Wheel {
+   func description() {
+      print("Wheel of Car B")
+   }
+}
+
+class CarFactoryA: CarFactory {
+   func createMotor() -> Motor {
+      return MotorA()
+   }
+
+   func createWheels() -> Wheel {
+      return WheelA()
+   }
+}
+
+class CarFactoryB: CarFactory {
+   func createMotor() -> Motor {
+      return MotorB()
+   }
+
+   func createWheels() -> Wheel {
+      return WheelB()
+   }
+}
+
+func main() {
+   let type: String = "A"
+   var factory: CarFactory
+   if type == "A" {
+      factory = CarFactoryA()
+   } else {
+      factory = CarFactoryB()
+   }
+   let motor: Motor = factory.createMotor()
+   let wheels: Wheel = factory.createWheels()
+
+   print("Cartype: " + type)
+   motor.description()
+   wheels.description()
+}
---
+++ swifting/Singelton.swift
@@ -0,0 +1,85 @@
+//
+//  Singelton.swift
+//  Swifting
+//
+//  Created by Julian Thaler on 05/03/24.
+//
+
+import Foundation
+import Swift
+
+main()
+
+func main() {
+   let logger: Logger = Logger.getInstance()
+   logger.setLogDateTime(logDateTime: true)
+   logger.logToConsole(message: "Console Logging")
+   logger.logToFile(message: "File Logging", path: "/Users/riven/MyProjects/swifting/logfile.txt")
+}
+
+extension Date {
+   static func getCurrentDate() -> String {
+      let dateFormatter: DateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "dd/MM/yyyy HH:mm:ss"
+      return dateFormatter.string(from: Date())
+   }
+}
+
+class Logger {
+   private static var INSTANCE: Logger!
+   private var logDateTime: Bool
+
+   private init() {
+		print("Created Signelton instance!")
+      logDateTime = true
+   }
+
+   public static func getInstance() -> Logger {
+      if INSTANCE == nil {
+         INSTANCE = Logger()
+      }
+      return INSTANCE
+   }
+
+   public func formatPrint(message: String) -> String {
+      var newMessage: String = message
+      if self.logDateTime {
+         newMessage = newMessage + " : " + Date.getCurrentDate()
+      }
+      return newMessage
+   }
+
+   public func setLogDateTime(logDateTime: Bool) {
+      self.logDateTime = logDateTime
+   }
+
+   public func logToConsole(message: String) {
+      print(formatPrint(message: message))
+   }
+
+   public func logToFile(message: String, path: String) {
+      let message2: String = formatPrint(message: message) + "\n"
+      let fileURL: URL = URL(fileURLWithPath: path)
+
+      if FileManager.default.fileExists(atPath: path) {
+         do {
+            let fileHandle: FileHandle = try FileHandle(forWritingTo: fileURL)
+            let currentPos: UInt64 = try fileHandle.seekToEnd()
+            if let data: Data = message2.data(using: .utf8) {
+               try fileHandle.seek(toOffset: currentPos)
+               fileHandle.write(data)
+               print("Logged to existing file!")
+            }
+         } catch {
+            print("ERROR logging to existing file!")
+         }
+      } else {
+         do {
+            try message2.write(to: fileURL, atomically: true, encoding: .utf8)
+            print("Logged to new file!")
+         } catch {
+            print("Error logging to new file!")
+         }
+      }
+   }
+}
